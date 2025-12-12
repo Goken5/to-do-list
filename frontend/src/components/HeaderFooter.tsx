62a992309ba4c0a545b81ea38e27bb24e68cf992
Davi Fernandes <fdavipessanha@gmail.com>
2025-12-12 09:59:06 -0300

    consertando erro de header sendo sobreposto

--- frontend/src/components/HeaderFooter.tsx
+++ frontend/src/components/HeaderFooter.tsx
@@ -1,7 +1,7 @@
 export function Header(){
 
     return(
-        <header className="fixed top-0 flex flex-col h-[50px] bg-blue-900 text-center w-full justify-center p-5 shadow-2xl">
+        <header className="sticky top-0 flex flex-col h-10 bg-blue-900 text-center w-full justify-center p-5 shadow-2xl z-50">
             <div>
                 <h1 className="margin-5 text-white text-2xl">To-do List</h1>
             </div>
@@ -10,7 +10,7 @@ export function Header(){
 }
 export function Footer(){
     return(
-        <footer className="fixed bottom-0 flex flex-col h-10 bg-blue-900 text-center w-full justify-center">
+        <footer className="sticky bottom-0 flex flex-col h-10 bg-blue-900 text-center w-full justify-center z-50">
             <div>
                 <h1 className="margin-5 text-white text-2xl">Crie sua lista de Tarefas</h1>
             </div>
